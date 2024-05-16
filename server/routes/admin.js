59a30fa464f9328209b9e23f819841c487d01b36
erica <erica.sanchez2835@yahoo.com>
2024-05-15 19:25:18 -0500

    login working satisfactory at the moment, beginnning work on dashboard/channels

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -4,6 +4,7 @@ const Post = require('../models/Post');
 const User = require('../models/User');
 
 const adminLayout = '../views/layouts/admin';
+const jwtSecret = process.env.JWT_SECRET;
 
 // admin - login page
 router.get('/admin', async (req, res) => {
@@ -20,16 +21,57 @@ router.get('/admin', async (req, res) => {
 });
 
 // admin - check login
+
 router.post('/admin', async (req, res) => {
     try {
         const { username, password } = req.body;
-        console.log(req.body);
-        res.redirect('/admin');
+        const user = await User.findOne( {username} );
+
+        if(!user) {
+            return res.status(401).json( {message: 'Invalid credentials' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid) {
+            return res.status(401).json( {message: 'Invalid credentials' });
+        }
+        
+        const token = jwt.sign( {userId: user._id}, jwtSecret )
+        res.cookie('token', token, { httpOnly: true })
+
+        res.redirect('/dashboard');
+
     } catch (error) {
         console.log(error);
     }
 });
 
+// dashboard 
+router.get('/dashboard', async (req, res) => {
+
+    res.render('admin/dashboard');
+});
+
+// register
+
+// router.post('/register', async (req, res) => {
+//     try {
+//         const { username, password } = req.body;
+//         const hashedPassword = await bcrypt.hash(password, 10);
+
+//         try {
+//             const user = await User.create({ username, password: hashedPassword});
+//             res.status(201).json({ message: 'User Created', user })
+//         } catch (error) {
+//             if (error.code === 11000) {
+//                 res.status(409).json({ message: 'User already in use' });
+//             }
+//             res.status(500).json({ message: 'Internal server error' })
+//         }
+//     } catch (error) {
+//         console.log(error);
+//     }
+// });
 
 
 module.exports = router;
