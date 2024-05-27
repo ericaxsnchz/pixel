3486194005022e36220d2a22d50e7b2c23499453
erica <erica.sanchez2835@yahoo.com>
2024-05-27 17:33:05 -0500

    updated db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
-            userNerUrlParser: true,
+            userNewUrlParser: true,
             useUnifiedTopology: true,
         });
         console.log('MongoDB connected');
